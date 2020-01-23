810b117f61899c1ef0c8fa3cc0a0d71316eeba5a
Magefan <ihoronesuch@gmail.com>
2020-01-23 15:04:17 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -3,8 +3,7 @@
  * Copyright © Magefan (support@magefan.com). All rights reserved.
  * Please visit Magefan.com for license details (https://magefan.com/end-user-license-agreement).
  */
-?>
-<?php
+
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Magefan_DSUClient',
